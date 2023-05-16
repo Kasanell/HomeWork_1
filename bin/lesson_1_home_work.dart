ec60dfa34e5403d84c8994390b9c5367430be0d4
kasanell <bredxilx@gmail.com>
2023-05-16 17:02:11 +0600

    first commit

---
+++ bin/lesson_1_home_work.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  String name, surename;
+  name = "Eldar";
+  surename = "Smanov";
+  print(name + " " + surename);
+}
---
+++ test/lesson_1_home_work_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_1_home_work/lesson_1_home_work.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
